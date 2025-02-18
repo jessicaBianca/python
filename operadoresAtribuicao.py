a0aff2dc7ce56a4a3614af43c0ce5ac4a2603bf9
Jessica <jessica.britto@outlook.com>
2025-02-18 14:34:36 -0300

    [Jessica Oliveira] Add operadoresAtribuicao.py

---
+++ operadoresAtribuicao.py
@@ -0,0 +1,19 @@
+#Operador de atribuição = usados para definir valor inicial ou sobreescrever valor de variavel
+
+#Atribuição simples
+idade = 17
+print(idade)
+
+#Atribuição com adição
+saldo = 500
+saldo += 200
+
+print(saldo)
+
+#outra forma de leitura 
+total = 500
+total = total + 200
+
+print(total)
+
+#posso usar tambem => -= , *=, /=, //=  [sem ponto flutuante depois de dividir] , %= , **=

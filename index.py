e9e6f04e177dc71f641218751ced9f714850adfb
Jessica <jessica.britto@outlook.com>
2025-02-18 12:18:03 -0300

    [Jessica Oliveira] Add arquivo operadoresAritmeticos.py

--- index.py
+++ index.py
@@ -1,1 +1,18 @@
 print("Olá")
+
+nome = "jessica"
+print(type(nome))
+
+#Apresenta erro - print(float(nome))
+
+#converte
+numero = 10
+print(float(numero))
+
+
+if(numero >=10):
+    print("Número OK")
+elif(numero<10):
+    print("Número NOK")
+
+
---
+++ operadoresAritmeticos.py
@@ -0,0 +1,58 @@
+# adição subtração multiplicação
+numeroUm = 10
+numeroDois = 2
+
+#adição
+resultado = numeroUm + numeroDois
+print(resultado)
+
+#subtração
+resultado = numeroUm - numeroDois
+print(resultado)
+
+#multiplicação
+resultado = numeroUm * numeroDois
+print(resultado)
+
+# divisão e divisão inteira
+numeroDivUm = 12
+
+#divisão
+print(numeroDivUm / 3)
+
+
+#divisão inteira (tira o float)
+print(numeroDivUm // 2)
+
+
+#Módulo e exponenciação
+
+#Módulo = resto da divisão do número
+print(numeroUm % 3)
+
+#Exponenciação (potência) - 2**3 - 2 elevado a 3 = 2 * 2 * 2 = 8 
+resultado = 2 ** 3
+print(resultado)  # Vai exibir 8
+
+
+#Precendência dos operadores
+
+
+# A precedência dos operadores define a ordem em que as operações matemáticas
+# são realizadas em uma expressão. No caso das operações matemáticas comuns,
+# a ordem de precedência é a seguinte:
+
+# Parênteses (): Operações dentro de parênteses são realizadas primeiro.
+# Exponenciação ^ ou **: A exponenciação é realizada antes das multiplicações,
+# divisões e adições.
+# Multiplicação * e Divisão /: São realizadas da esquerda para a direita.
+# Soma + e Subtração -: São realizadas por último, também da esquerda para a direita.
+
+# A ordem de precedência pode ser lembrada com a sigla PEMDAS, que significa:
+
+# P - Parênteses
+# E - Exponenciação
+# M - Multiplicação
+# D - Divisão
+# A - Adição
+# S - Subtração

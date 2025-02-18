37edc8673a9208d771f31f5dc2576e86e0a91a75
Jessica <jessica.britto@outlook.com>
2025-02-18 12:22:12 -0300

    [Jessica Oliveira] Add arquivo operadoresComparacao.py

---
+++ operadoresComparacao.py
@@ -0,0 +1,16 @@
+#operadores utilizados para comparar dois valores 
+saldo = 450
+saque = 200
+
+print(saldo == saque)
+
+print(saldo != saque)
+
+print(saldo > saque)
+
+print(saldo >= saque)
+
+print(saldo < saque)
+
+print(saldo <= saque)
+
